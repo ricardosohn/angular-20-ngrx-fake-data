d763123337f57725b3cf2e19bee1bb3fcb361e24
Ricardo Sohn <ricardosohn@gmail.com>
2025-10-17 10:48:57 -0300

    feat: adicionado ngrx e melhorada estrutura do projeto (foco no dominio)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
@@ -7,6 +10,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: false })
   ]
 };
